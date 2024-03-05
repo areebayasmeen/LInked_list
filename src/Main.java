b827f3020bec4836d7342513c3a067f56e856168
areebayasmeen <areebayasmeen11@gmail.com>
2024-03-05 17:52:51 +0500

    Linked_List

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        public static void main(String[] args) {
+            Linked_list list = new Linked_list();
+            list.add("1");
+            list.add("2");
+            list.add("3");
+            list.add("4");
+            list.add("5");
+            list.add("6");
+            list.display();
+            list.add(1,"101");
+            list.display();
+            list.add(9,"3");
+            list.display();
+        }
     }
-}
+
